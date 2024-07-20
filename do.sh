e6349bb4322ff115500c6dd568b7ec2e888d5e1d
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-07-20 14:27:44 +0300

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 
-
-sudo apt-get update && sudo apt-get upgrade -y
-
-
-sudo apt-get install python3-pip ffmpeg -y
-
-
-sudo apt-get install python3-pip -y
-
-
-sudo pip3 install -U pip
-
-
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
-sudo apt-get install nodejs -y
-sudo npm i -g npm
-
-
 cd up && pip3 install -U -r requirements.txt
 
 
